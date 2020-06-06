6925efa0d1748e9f48b43c10e229eaa13a49cf7b
tekkenthuuug <sa.soto317@gmail.com>
2020-06-06 21:53:58 +0200

    Removing Redux and related to Redux libraries from package.json, removing imports in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 import { ApolloProvider } from 'react-apollo';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
@@ -10,8 +8,6 @@ import { ApolloClient } from 'apollo-boost';
 
 import data from './graphql/initial-data';
 import { resolvers, typeDefs } from './graphql/resolvers';
-
-import { store, persistor } from './redux/store';
 
 import { default as App } from './App/App.container';
 
@@ -34,13 +30,9 @@ client.writeData({
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Provider store={store}>
-      <BrowserRouter>
-        <PersistGate persistor={persistor}>
-          <App />
-        </PersistGate>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ApolloProvider>,
   document.getElementById('root')
 );
